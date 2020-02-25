edea4bd00423c3b41bbd2efb5d52598424901d60
lyldev <lyl_dev@outlook.com>
2020-02-25 12:20:36 +0800

    docs(personalUrl): modify chromeExtensionUrl to [notavailable]

--- src/common/personalUrls.js
+++ src/common/personalUrls.js
@@ -5,5 +5,5 @@ const suffix = browserName === "Chrome" ? "fc" : "";
 export const email = `xxx.firefox+st${suffix}@gmail.com`;
 export const paypalLink = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&no_shipping=1&business=sienori.firefox@gmail.com&item_name=Yi Translate for ${browserName} - Donation`;
 export const patreonLink = "https://www.patreon.com/xxx";
-export const chromeExtensionUrl = `https://chrome.google.com/webstore/detail/ibplnjkanclpjokhdolnendpplpjiace`;
+export const chromeExtensionUrl = `https://chrome.google.com/webstore/detail/notavailablenow`;
 export const firefoxAddonUrl = `https://addons.mozilla.org/firefox/addon/yi-translate/`;
