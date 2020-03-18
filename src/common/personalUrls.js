180c55da2c2f50639587c4e88540aacead9c17f0
lyldev <lyl_dev@outlook.com>
2020-03-18 16:30:33 +0800

    refact(personalUrl): add several donate links

--- src/common/personalUrls.js
+++ src/common/personalUrls.js
@@ -2,8 +2,14 @@ import browserInfo from "browser-info";
 
 const browserName = browserInfo().name;
 const suffix = browserName === "Chrome" ? "fc" : "";
-export const email = `xxx.firefox+st${suffix}@gmail.com`;
-export const paypalLink = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&no_shipping=1&business=sienori.firefox@gmail.com&item_name=Yi Translate for ${browserName} - Donation`;
-export const patreonLink = "https://www.patreon.com/xxx";
+export const email = `lyl_dev@outlook.com`;
+
+// donate link
+export const paypalLink = `https://paypal.me/lyldev`;
+export const patreonLink = "https://www.patreon.com/lyldev";
+export const buymeacoffeeLink = `https://www.buymeacoffee.com/lyldev`;
+export const chongyaLink = `https://www.chongya.com/@lyldev`;
+export const afdianLink = `https://www.afdian.com/@lyldev`;
+// addon url
 export const chromeExtensionUrl = `https://chrome.google.com/webstore/detail/notavailablenow`;
 export const firefoxAddonUrl = `https://addons.mozilla.org/firefox/addon/yi-translate/`;
