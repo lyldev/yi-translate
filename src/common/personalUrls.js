0f17c45ec607552d4e12c7b0bca02d20388e5de1
lyldev <lyl_dev@outlook.com>
2020-03-18 17:22:39 +0800

    fix(typos): fix afdian link typos

--- src/common/personalUrls.js
+++ src/common/personalUrls.js
@@ -4,12 +4,12 @@ const browserName = browserInfo().name;
 const suffix = browserName === "Chrome" ? "fc" : "";
 export const email = `lyl_dev@outlook.com`;
 
-// donate link
+// sponsor link
 export const paypalLink = `https://paypal.me/lyldev`;
 export const patreonLink = "https://www.patreon.com/lyldev";
 export const buymeacoffeeLink = `https://www.buymeacoffee.com/lyldev`;
 export const chongyaLink = `https://www.chongya.com/@lyldev`;
-export const afdianLink = `https://www.afdian.com/@lyldev`;
+export const afdianLink = `https://afdian.net/@lyldev`;
 // addon url
 export const chromeExtensionUrl = `https://chrome.google.com/webstore/detail/notavailablenow`;
 export const firefoxAddonUrl = `https://addons.mozilla.org/firefox/addon/yi-translate/`;
